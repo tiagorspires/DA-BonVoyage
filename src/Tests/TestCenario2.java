fe41a74161ca1da37d8ae22f7bd4066e93c45c17
up201808613 <up201808613@fc.up.pt>
2022-06-06 17:19:47 +0100

    Revert "Revert "only 2.5 missing. Ricardo about to blow this shit up with an unnecessary refactor""
    This reverts commit 451588d637bbde7916b9ebb62081ebe06c5290d1.

--- src/Tests/TestCenario2.java
+++ src/Tests/TestCenario2.java
@@ -3,6 +3,8 @@ package Tests;
 import Cenario1.Cenario1a;
 import Cenario1.Cenario1b;
 import Cenario2.Cenario2a;
+import Cenario2.Cenario2c;
+import Cenario2.Cenario2d;
 import GraphManager.Edge;
 import GraphManager.Graph;
 
@@ -15,6 +17,7 @@ public class TestCenario2 {
     public static void Execute(){
         Graph graph = new Graph(6);
         /*
+        test case for cenario 1b
         graph.addEdge(0,1,4);
         graph.addEdge(0,2,3);
         graph.addEdge(1,3,5);
@@ -23,19 +26,20 @@ public class TestCenario2 {
         graph.addEdge(3,2,2);
         graph.addEdge(3,4,4);
         */
-        graph.addEdge(0,1,4);
-        graph.addEdge(0,2,4);
-        graph.addEdge(1,3,3);
-        graph.addEdge(2,4,2);
-        graph.addEdge(3,5,3);
-        graph.addEdge(4,5,3);
+        graph.addEdge(0,1,4, 4);
+        graph.addEdge(0,2,4, 5);
+        graph.addEdge(1,3,3, 1);
+        graph.addEdge(2,4,2, 6);
+        graph.addEdge(3,5,3, 7);
+        graph.addEdge(4,5,3, 3);
 
 
 
-        Cenario2a.execute(graph,0,5,5);
+        Cenario2d.execute(graph,0,5);
         System.out.println("Expected Values");
-        System.out.println("[[0, 1, 3, 5], [0, 2, 4, 5]]");
         System.out.println("5");
+        System.out.println("[[0, 1, 3, 5], [0, 2, 4, 5]]");
+
 
 
     }
